8f971bf74ff0d7cbd93f4ca9b0e9003b03327b59
RummenacherK <Krummenacher@live.com>
2017-01-26 20:47:56 -0600

    Lesson3SchoolListProject

---
+++ 03Lesson/Lesson3InClassProject/src/Lesson3InClassProject/Controllers/StudentController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Lesson3InClassProject.Models;
+
+
+namespace Lesson3InClassProject.Controllers
+{
+    public class StudentController : Controller
+    {
+        public IActionResult Index()
+        {
+            Levels intro = new Levels() { Id = 1, Name = "intro" };
+            Levels intermediate = new Levels() { Id = 2, Name = "intermediate" };
+            Levels advanced = new Levels() { Id = 3, Name = "advanced" };
+
+            Student Fred = new Student() { Id = 1, Name = "Fred", Levels = intro };
+            Student Nancy = new Student() { Id = 2, Name = "Nancy", Levels = intermediate };
+            Student John = new Student() { Id = 3, Name = "John", Levels = advanced };
+
+          
+            
+
+            List<Student> StudentList = new List<Student>();
+            StudentList.Add(Fred);
+            StudentList.Add(Nancy);
+            StudentList.Add(John);
+            
+            return View(StudentList);
+        }
+    }
+}
---
+++ 03Lesson/Lesson3InClassProject/src/Lesson3InClassProject/Models/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Lesson3InClassProject.Models
+{
+    public class Student
+    {
+        public int Id;
+        public string Name { get; set; }
+        public int LevelsId { get; set; }
+
+        public virtual Levels Levels { get; set; }
+    }
+}
