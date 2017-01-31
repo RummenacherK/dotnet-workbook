785b730f0d2a843fbb4d625b480d00adf3c89826
RummenacherK <Krummenacher@live.com>
2017-01-30 20:46:56 -0600

    LanguageFeaturesSideWork

---
+++ 02Lesson/LanguageFeatures/src/LanguageFeatures/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using LanguageFeatures.Models;
+
+
+namespace LanguageFeatures.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ViewResult Index()
+        {
+            ShoppingCart cart = new ShoppingCart
+            {
+                Products = Product.GetProducts()
+            };
+            Product[] productArray =
+             {
+                new Product {Name = "Kayak", Price = 275M},
+                new Product {Name = "Lifejacket", Price = 48.95M}
+            };
+
+            decimal cartTotal = cart.TotalPrices();
+            decimal arrayTotal = productArray.TotalPrices();
+
+            return View("Index", new string[]
+            {
+                $"Cart Total: {cartTotal:C2}",
+                $"Array Total: {arrayTotal:C2}"
+            });
+        }
+    }
+}
---
+++ 02Lesson/LanguageFeatures/src/LanguageFeatures/Models/ShoppingCart.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace LanguageFeatures.Models
+{
+    public class ShoppingCart : IEnumerable<Product> 
+    {
+        public IEnumerable<Product> Products { get; set; }
+
+        public IEnumerator<Product> GetEnumerator()
+        {
+            return Products.GetEnumerator();
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return GetEnumerator();
+        }
+    }
+}
